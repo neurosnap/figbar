5815a7e93496b826c19c8a95c4b1ba2cc3080955
Eric Bower <me@erock.io>
2026-09-08 09:22:05 -0400

    chore: remove c files

--- src/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-#include "state.h"
-
-void render(void *data, struct state *state);
-
-#endif
--- src/shm.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHM_H
-#define SHM_H
-
-#include <sys/types.h>
-
-int allocate_shm_file(size_t size);
-
-#endif
